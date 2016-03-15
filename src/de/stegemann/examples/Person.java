dcee3c39c4c41e5c8bdaeaf25eec8aa49b5542f0
xXGrippSXx <grippshd@gmail.com>
2016-03-15 07:57:50 +0100

    Übung mir Arne: hash Methode durch eclipse

--- src/de/stegemann/examples/Person.java
+++ src/de/stegemann/examples/Person.java
@@ -6,4 +6,32 @@ public class Person {
 	public Person(String name) {
 		this.name = name;
 	}
+
+	// Von eclipse generiert mit Source -> Generate hashCode() and equals() aus
+	// Kontextmenü
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
 }
