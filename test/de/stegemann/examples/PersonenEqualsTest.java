77246e4cb0c22bc7f43297034e007dd73f3e94f4
xXGrippSXx <grippshd@gmail.com>
2016-03-15 07:55:00 +0100

    Übung mit Arne: Problem Die Personen sind nicht gleich

--- test/de/stegemann/examples/PersonenEqualsTest.java
+++ test/de/stegemann/examples/PersonenEqualsTest.java
@@ -1,5 +1,6 @@
 package de.stegemann.examples;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
@@ -11,5 +12,13 @@ public class PersonenEqualsTest {
 		Person person2 = new Person("Arne");
 
 		assertNotEquals(person1, person2);
+	}
+
+	@Test
+	public void identischePersonenSindEqual() throws Exception {
+		Person person1 = new Person("Jan");
+		Person person2 = new Person("Jan");
+
+		assertEquals(person1, person2);
 	}
 }
